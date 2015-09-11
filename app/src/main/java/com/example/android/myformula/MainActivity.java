0306b629852e772e52f4ee7e47d5a8c4c77ff953
manaday.mavani <m.k.mavani@gmail.com>
2015-09-11 19:54:16 +0530

    reference links

--- app/src/main/java/com/example/android/myformula/MainActivity.java
+++ app/src/main/java/com/example/android/myformula/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.myformula;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -10,7 +10,13 @@ import com.example.android.myformula.adapter.FormulaListAdapter;
 import com.example.android.myformula.model.Formula;
 
 import java.util.ArrayList;
-import java.util.List;
+
+/**
+ * http://stackoverflow.com/questions/11945563/how-listviews-recycling-mechanism-works
+ * http://stackoverflow.com/questions/19469073/how-do-you-efficiently-load-bitmaps-from-drawable-folder-into-a-listview/19469076#19469076
+ * http://www.pcsalt.com/android/listview-using-baseadapter-android/
+ * http://www.androidhive.info/2014/07/android-custom-listview-with-image-and-text-using-volley/
+ */
 
 public class MainActivity extends AppCompatActivity {
 
